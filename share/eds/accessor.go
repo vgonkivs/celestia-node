bd4596250c42c6d357c89a2837a2178da78c5370
vgonkivs <viacheslavgonkivskyi@gmail.com>
2025-04-28 17:35:30 +0300

    move AxisHalf under shwap package

--- share/eds/accessor.go
+++ share/eds/accessor.go
@@ -11,6 +11,8 @@ import (
 	"github.com/celestiaorg/celestia-node/share/shwap"
 )
 
+var _ shwap.Accessor = (AccessorStreamer)(nil)
+
 // EmptyAccessor is an accessor of an empty EDS block.
 var EmptyAccessor = &Rsmt2D{ExtendedDataSquare: share.EmptyEDS()}
 
